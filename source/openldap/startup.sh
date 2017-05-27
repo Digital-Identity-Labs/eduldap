5952a13ea3e9fbd35a5b6cb1a1e9e3f1e1951bef
Pete Birkinshaw <pete@binary-ape.org>
2017-05-27 16:10:45 +0100

    Adjust seeds and databases - can't do both together

--- source/openldap/startup.sh
+++ source/openldap/startup.sh
@@ -1,40 +1,61 @@
 #!/bin/sh
 
 CONFIG_DIR=/var/lib/openldap/slapd.d
+DATA_DIR=/var/lib/openldap/openldap-data
 ETC=/etc/openldap
+
 PASSWORD=$1
+DEBUG_LEVEL=0
+DATABASE=default
+SEED=default
 
 ## Configuration data will be stored here
 mkdir -p $CONFIG_DIR
 chown -R ldap:ldap $CONFIG_DIR
 
 if [ ! -f "$CONFIG_DIR/cn=config.ldif" ]; then
-  echo "Setting up initial LDAP server configuration..."
+  echo "No LDAP configuration has been found. Initialising new configuration!"
+  echo ":: Setting up core LDAP server configuration"
   slapadd -n0 -F $CONFIG_DIR -l $ETC/slapd.ldif
 
-  echo "Importing schema:"
+  echo ":: Importing schema:"
   for file in $ETC/schema/*.ldif
   do
-    echo " $file"
+    echo "    $file"
     slapadd -n0 -F $CONFIG_DIR -l $file
   done
 
   slapadd -n0 -F $CONFIG_DIR -l $ETC/modules.ldif
-  slapadd -n0 -F $CONFIG_DIR -l $ETC/access.ldif
 
-  echo "Available databases:"
+  echo ":: Available databases:"
   for file in $ETC/databases/*.ldif
   do
-    echo " $file"
+    echo "    $file"
   done
 
-  #slapadd -n0 -F $CONFIG_DIR -l $ETC/password.ldif
+  echo ":: Adding database definition, ACLs, etc from $DATABASE.ldif"
+  slapadd -n0 -F $CONFIG_DIR -l $ETC/databases/$DATABASE.ldif
 
+  echo ":: Testing configuration..."
+  #/usr/sbin/slapd  -T t  -u ldap -F $CONFIG_DIR
+
+  echo ":: Available seed data:"
+  for file in $ETC/seeds/*.ldif
+  do
+    echo "    $file"
+  done
+
+  echo ":: Loading seed data from $SEED.ldif"
+  slapadd -n1 -F $CONFIG_DIR -l $ETC/seeds/$SEED.ldif
+
+  echo ":: Post configuration adjustments"
   slapadd -n0 -F $CONFIG_DIR -l $ETC/extras.ldif
 
-
+  chown -R ldap:ldap $CONFIG_DIR
+  chown -R ldap:ldap $DATA_DIR/*
 fi
 
-exec /usr/sbin/slapd -d 389 -u ldap -g ldap
+echo "Starting LDAP service..."
+exec /usr/sbin/slapd -F $CONFIG_DIR -d $DEBUG_LEVEL -u ldap -g ldap
 
 
